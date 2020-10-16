8f88da851acc28afdecc7361a71d3b5c49b0b129
黄炜杰 <wjhuang@live.cn>
2020-10-16 20:57:14 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ mod repository;
 use actix_web::{App, HttpServer};
 use controller::{hello, posts, post};
 
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
